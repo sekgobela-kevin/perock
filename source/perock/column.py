1766e3760f9dbecf6d611b9f4e59671ea6801470
Kevin Sekgobela <kevinnoko23@gmail.com>
2022-06-22 15:35:01 +0200

    Removed credentials.py and column.py
    Closes #2
    
    credentials.py and column.py are no longer relevant to the project.
    See issue #2 for more details.

--- source/perock/column.py
+++
@@ -1,8 +0,0 @@
-'''
-Defines fuctionality for operating with column data.
-Column can refer things like 'passwords' or 'usenames'.
- 
-Author: Sekgobela Kevin
-Date: June 2022
-Languages: Python 3
-'''
