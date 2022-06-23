c1c34dc27c81ae6d2c110f620b5a185ba8b7639d
Kevin Sekgobela <kevinnoko23@gmail.com>
2022-06-23 08:01:18 +0200

    Added __init__.py and perock.py

---
+++ source/perock/__init__.py
@@ -0,0 +1,13 @@
+from attack import Attack
+from attack import AttemptAsync
+
+from forcetable import FColumn
+from forcetable import FRow
+from forcetable import FTable
+
+from bruteforce import BForce
+from bruteforce import BForceAsync
+
+from perock import read_file_lines
+from perock import FColumnFile
+from perock import file_to_column
---
+++ source/perock/perock.py
@@ -0,0 +1,26 @@
+import os
+import forcetable
+
+
+def read_file_lines(path):
+    '''Reads lines from file in path, returns generator'''
+    with open(path, "r") as f:
+        #yield from f
+        for line in f:
+            yield line.strip()
+
+def file_to_column(path, name=None):
+    '''Creates FColumn object from file in path'''
+    items = read_file_lines(path)
+    if name == None:
+        # filename with extension
+        filename = os.path.split(path)
+        # filename without extension
+        name = os.path.splitext(filename)[0]
+    return forcetable.FColumn(name, items)
+
+class FColumnFile(forcetable.FColumn):
+    '''Class for creating column items from file in path''' 
+    def __init__(self, name, path) -> None:
+        file_lines = read_file_lines(path)
+        super().__init__(name, file_lines)
