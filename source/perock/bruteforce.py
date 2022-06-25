89bed98b82af560fb5faf5fe05705071eca653ee
Sekgobela-Kevin <kevinnoko23@gmail.com>
2022-06-25 18:16:53 +0200

    WebAttack and WebAttack async moved to attacks.py
    attacks.py file/modukle was created and the classes were added to it.
    Existing import statement that were depending on the classes were also
    modified to refer to attacks module instead of attack module.
    
    Classes in attacks.py are meant to be used by users/consumers of perock
    not the library itself. The classes may require certain libraries like
    requests. It will be the consumers of the library responsibility to install
    them.
    
    This commit is related to closed issue #7.

--- source/perock/bruteforce.py
+++ source/perock/bruteforce.py
@@ -19,8 +19,6 @@ from concurrent.futures import ThreadPoolExecutor
 
 from .attack import Attack
 from .attack import AttackAsync
-from .attack import WebAttackAsync
-from .attack import WebAttack
 
 from .forcetable import FColumn
 from .forcetable import FRow
